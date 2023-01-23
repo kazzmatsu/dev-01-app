930cc641d06d4458164579afc1e702f4d01ba43c
kazzmatsu <kazmatsu@outlook.com>
2023-01-23 12:10:23 +0900

    third Change

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,9 @@ export default function Home() {
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
+            Kaz Matsu
+          </div>
+          {/* <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
@@ -116,7 +119,7 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
     </>
   )
